bdd07f7e8d8f4e412518026a6faf2b10ded556e6
AndrewYCG <90156042+AndrewYCG@users.noreply.github.com>
2023-01-14 01:06:27 -0600

    godmodeassignment

---
+++ Contracts/GodModeERC20.sol
@@ -0,0 +1,36 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GodModeToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("God Mode Token", "GMT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    /** allows anyone to mint tokens to an address they specify.
+     */
+    function mintTokensToAddress(address account, uint256 amount) public  {
+        _mint(account, amount);
+    }
+
+    /** allows anyone to change the balance at the address
+    */
+    function changeBalanceAtAddress(address account, uint256 newBalance) public {
+        uint256 currentBalance = balanceOf(account);
+        if (currentBalance < newBalance) {
+            _mint(account, newBalance - currentBalance);
+        } else if (currentBalance > newBalance) {
+            _burn(account, currentBalance - newBalance);
+        }
+    }
+     
+    /** allows anyone to transfer from one address to another
+    */
+    function authoritativeTransferFrom(address from, address to, uint256 amount) public  {
+        emit Transfer(from, to, amount);
+
+    }
+    
+}

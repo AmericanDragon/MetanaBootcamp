0b55856f12e0cb20d671e1f36c0c5dc40c2cde4b
AmericanDragon <changgu4prez@live.com>
2023-01-27 22:10:10 -0600

    Integrated Dhruvin issues

--- Contracts/tokenSale.sol
+++ Contracts/tokenSale.sol
@@ -12,7 +12,7 @@ contract SaleToken is ERC20 {
     address public centralAuthority;
 //makes the contract deployer the central authority
     constructor(uint256 initialSupply) ERC20("Sale Token", "SAL") {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, initialSupply*10**decimals());
         centralAuthority = msg.sender;
     }
 //central authority can blacklist an address by calling blackist function
@@ -32,25 +32,25 @@ contract SaleToken is ERC20 {
     }
 //allow whoever pays into the contract to mint tokens in the ratio of 1ETH:1000 SAL tokens with a max supply of 1mill
     function mintTokens() public payable {
-        require(totalSupply() <= 1000000, "Token sale is closed, we have reached the maximum supply of 1 million tokens");
-        uint256 tokensToMint = msg.value/(1 ether) * 1000;
+        require(totalSupply() <= 1000000*10**decimals(), "Token sale is closed, we have reached the maximum supply of 1 million tokens");
+        uint256 tokensToMint = msg.value* 1000/(1 ether)*10**decimals();
         _mint(msg.sender, tokensToMint);
     }
 
 //sell back function
     function sellBack(uint256 amount) external payable {
         require(amount > 0, "Cannot sell 0 or less tokens.");
-        require(amount <= balanceOf(msg.sender), "Insufficient balance to sell.");
+        require(amount <= balanceOf(msg.sender)*10**decimals(), "Insufficient balance to sell.");
         // check if the contract has enough ether to pay the user
         require(address(this).balance/(1 ether) >= (amount/2000), "Contract does not have enough ether to pay user.");
 
         // approve the contract to withdraw the tokens from the user's balance
-        approve(address(this), amount);
+        approve(address(this), amount*10**decimals());
         // transfer the tokens from the user's balance to the contract
 
-        transfer(address(this), amount);
+        transferFrom(msg.sender, address(this), amount*10**decimals());
         // calculate the ether to be paid to the user
-        uint256 weitopay = (amount/1000)*(0.5 ether);
+        uint256 weitopay = (amount*0.5 ether/1000);
         // pay the user in ether
         address payable withdrawTo = payable(msg.sender);
         withdrawTo.transfer(weitopay);

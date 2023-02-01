dcba03e3dda01662682f160a9e6ae61b553dbb90
AmericanDragon <changgu4prez@live.com>
2023-01-31 22:55:57 -0600

    NFT Staking Rewards Finished

---
+++ Contracts/new-OZ-nft.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPT-3.0
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OpenZeppelinNFT is ERC721, Ownable {
+
+    uint256 public tokenSupply = 0;
+    uint256 public constant MAX_SUPPLY = 10;
+    uint256 public constant PRICE = 0 ether;
+
+    address immutable deployer;
+
+    constructor() ERC721("Fast Fighter Jets", "FFJ"){
+        deployer = msg.sender;
+
+    }
+
+    function mint() external payable {
+        require(tokenSupply < MAX_SUPPLY,"supply used up");
+        require(msg.value == PRICE, "wrong price");
+        _mint(msg.sender, tokenSupply);
+        tokenSupply++;
+
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmSpbhLRQsr1PZmcdCJXrwhesj4hsiEY3t9o79xpAoVV4V/";
+    }
+
+    function viewBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdraw() external {
+        payable(deployer).transfer(address(this).balance);
+    }
+    //function renounceOwnership() public override  {
+    //    require(false, "cannot renounce");
+    //}
+    //function transferOwnership(address newOwner) public override  {
+    //    require(false, "cannot transfer");
+    //}
+
+}
---
+++ Contracts/nftStakingfromScratch.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: MIT LICENSE
+
+pragma solidity ^0.8.7;
+
+import "new-OZ-nft.sol";
+import "stakingRewardsToken.sol";
+
+
+contract NFTStaking is Ownable, IERC721Receiver {
+    //total number of nfts staked by a particular user
+    uint256 public totalStaked;
+
+    uint256 constant SecondsPerDay = 10;
+
+    // struct to store a stake's token, owner, and earning values
+    struct Stake {
+        uint24 tokenId;
+        uint48 timestamp;
+        address owner;
+    }
+
+    event NFTStaked(address owner, uint256 tokenId, uint256 value);
+    event NFTUnstaked(address owner, uint256 tokenId, uint256 value);
+    event Claimed(address owner, uint256 amount);
+
+    // reference to the Block NFT contract
+    OpenZeppelinNFT nft;
+    StakeToken token;
+
+    // maps tokenId to stake
+    mapping(uint256 => Stake) public vault;
+
+    //create a constructor that will create 2 functions, the nft and the token
+    constructor(OpenZeppelinNFT _nft, StakeToken _token) {
+        nft = _nft;
+        token = _token;
+    }
+
+    function stake(uint256[] calldata tokenIds) external {
+        uint256 tokenId;
+        totalStaked += tokenIds.length;
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            tokenId = tokenIds[i];
+            require(nft.ownerOf(tokenId) == msg.sender, "not your token");
+            require(vault[tokenId].tokenId == 0, "already staked");
+
+            nft.transferFrom(msg.sender, address(this), tokenId);
+            emit NFTStaked(msg.sender, tokenId, block.timestamp);
+
+            vault[tokenId] = Stake({
+                owner: msg.sender,
+                tokenId: uint24(tokenId),
+                timestamp: uint48(block.timestamp)
+            });
+        }
+    }
+
+    function _unstakeMany(address account, uint256[] calldata tokenIds)
+        internal
+    {
+        uint256 tokenId;
+        totalStaked -= tokenIds.length;
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            tokenId = tokenIds[i];
+            Stake memory staked = vault[tokenId];
+            require(staked.owner == msg.sender, "not an owner");
+
+            delete vault[tokenId];
+            emit NFTUnstaked(account, tokenId, block.timestamp);
+            nft.transferFrom(address(this), account, tokenId);
+        }
+    }
+
+    function claim(uint256[] calldata tokenIds) external {
+        _claim(msg.sender, tokenIds, false);
+    }
+
+    function claimForAddress(address account, uint256[] calldata tokenIds)
+        external
+    {
+        _claim(account, tokenIds, false);
+    }
+
+    function unstake(uint256[] calldata tokenIds) external {
+        _claim(msg.sender, tokenIds, true);
+    }
+
+    function _claim(
+        address account,
+        uint256[] calldata tokenIds,
+        bool _unstake
+    ) internal {
+        uint256 tokenId;
+        uint256 earned = 0;
+
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            tokenId = tokenIds[i];
+            Stake memory staked = vault[tokenId];
+            require(staked.owner == account, "not an owner");
+            uint256 stakedAt = staked.timestamp;
+            //issue 10 token for every day
+            earned +=
+                (10 * 10**18 * (block.timestamp - stakedAt)) /
+                SecondsPerDay;
+            vault[tokenId] = Stake({
+                owner: account,
+                tokenId: uint24(tokenId),
+                timestamp: uint48(block.timestamp)
+            });
+        }
+        if (earned > 0) {
+            token.mint(account, earned);
+        }
+        if (_unstake) {
+            _unstakeMany(account, tokenIds);
+        }
+        emit Claimed(account, earned);
+    }
+
+    function onERC721Received(
+        address,
+        address from,
+        uint256,
+        bytes calldata
+    ) external pure override returns (bytes4) {
+        require(from == address(0x0), "Cannot send nfts to Vault directly");
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
